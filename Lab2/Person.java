016848bc51f3256b0024100098e8543864d26bd3
dar1acraciun <daria.craciun411@gmail.com>
2025-03-08 15:17:55 +0200

    delete lab2

--- Lab2/Person.java
+++
@@ -1,7 +0,0 @@
-import java.time.LocalDate;
-
-public abstract class Person {
-    protected String name;
-    protected LocalDate dateOfBirth;
-
-}
--- Lab2/ProjectType.java
+++
@@ -1,3 +0,0 @@
-public enum ProjectType {
-    THEORETHICAL,PRACTICAL;
-}
